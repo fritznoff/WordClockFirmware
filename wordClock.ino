e3ac3e13e095d31d63cdcc0ffa4955238ce72975
Janis <janis.tiemann@tu-dortmund.de>
2012-11-03 02:38:42 +0100

    Preparations for wordclock display abstraction

--- wordClock.ino
+++ wordClock.ino
@@ -2,6 +2,47 @@
 
 #define EE_WC_BR 0x20
 
+/*
+  there are actually 7bit needed, to describe the position of each led
+  3bit are the ic identifier and 4bit describe the actual led connected to the ic 
+  example:     
+   progmembyte     n/a   ic-id  led-id
+                   \_/   \___/  \____/
+   0b01010111:  0b  0     101    0111
+  
+  
+                original                                  hexadecimal
+  
+  *                                   *      6E                                  6F
+     E  S  K  I  S  T  L  F  Ü  N  F            00 01 02 03 04 05 06 07 08 09 0A
+     Z  E  H  N  Z  W  A  N  Z  I  G            0B 0C 0D 0E 0F 10 11 12 13 14 15
+     D  R  E  I  V  I  E  R  T  E  L            16 17 18 19 1A 1B 1C 1D 1E 1F 20
+     T  G  N  A  C  H  V  O  R  J  M            21 22 23 24 25 26 27 28 29 2A 2B
+     H  A  L  B  Q  Z  W  Ö  L  F  P            2C 2D 2E 2F 30 31 32 33 34 35 36
+     Z  W  E  I  N  S  I  E  B  E  N            37 38 39 3A 3B 3C 3D 3E 3F 40 41
+     K  D  R  E  I  R  H  F  Ü  N  F            42 43 44 45 46 47 48 49 4A 4B 4C
+     E  L  F  N  E  U  N  V  I  E  R            4D 4E 4F 50 51 52 53 54 55 56 57
+     W  A  C  H  T  Z  E  H  N  R  S            58 59 5A 5B 5C 5D 5E 5F 60 61 62
+     B  S  E  C  H  S  F  M  U  H  R            63 64 65 66 67 68 69 6A 6B 6C 6D
+  *                                   *      71                                  70
+  
+*/
+
+//to be done
+const uint8_t led_alignment[] PROGMEM = {
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10,
+  0x10, 0x10, 0x10, 0x10
+};
+
 //why uint16_t?
 const uint16_t log_pwm[32] PROGMEM =
 {
@@ -385,9 +426,4 @@ void showZWOELF() {
   newStates[5] ^= 0b0011110000000000;
 }
 
-/*
-  there are actually 7bit needed, to describe the position of each led
-  3bit are the ic identifier and 4bit describe the actual led connected to the ic 
 
-  therefore we need a matrix of 11x11 bits
-*/
