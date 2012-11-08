cc08dee73a58890d0a0ea50b0ada2d817a96518b
Janis <janis.tiemann@tu-dortmund.de>
2012-11-08 17:53:25 +0100

    Filled word arrays

--- wordClock.ino
+++ wordClock.ino
@@ -33,7 +33,7 @@
 */
 
 const uint8_t show_es[] PROGMEM = {0x00, 0x01};
-const uint8_t show_ist[] PROGMEM = {0x03, 0x04};
+const uint8_t show_ist[] PROGMEM = {0x03, 0x04, 0x05};
 const uint8_t show_vor[] PROGMEM = {0x36, 0x37, 0x38};
 const uint8_t show_nach[] PROGMEM = {0x32, 0x33, 0x34, 0x35};
 const uint8_t show_uhr[] PROGMEM = {0x98, 0x99, 0x9A};
@@ -42,19 +42,19 @@ const uint8_t show_zehn[] PROGMEM = {0x10, 0x11, 0x12, 0x13};
 const uint8_t show_viertel[] PROGMEM = {0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A};
 const uint8_t show_zwanzig[] PROGMEM = {0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A};
 const uint8_t show_halb[] PROGMEM = {0x40, 0x41, 0x42, 0x43};
-const uint8_t show_hour_ein[] PROGMEM = {};
-const uint8_t show_hour_eins[] PROGMEM = {};
-const uint8_t show_hour_zwei[] PROGMEM = {};
-const uint8_t show_hour_drei[] PROGMEM = {};
-const uint8_t show_hour_vier[] PROGMEM = {};
-const uint8_t show_hour_fuenf[] PROGMEM = {};
-const uint8_t show_hour_sechs[] PROGMEM = {};
-const uint8_t show_hour_sieben[] PROGMEM = {};
-const uint8_t show_hour_acht[] PROGMEM = {};
-const uint8_t show_hour_neun[] PROGMEM = {};
-const uint8_t show_hour_zehn[] PROGMEM = {};
-const uint8_t show_hour_elf[] PROGMEM = {};
-const uint8_t show_hour_zwoelf[] PROGMEM = {};
+const uint8_t show_hour_ein[] PROGMEM = {0x52, 0x53, 0x54};
+const uint8_t show_hour_eins[] PROGMEM = {0x52, 0x53, 0x54, 0x55};
+const uint8_t show_hour_zwei[] PROGMEM = {0x50, 0x51, 0x52, 0x53};
+const uint8_t show_hour_drei[] PROGMEM = {0x61, 0x62, 0x63, 0x64};
+const uint8_t show_hour_vier[] PROGMEM = {0x77, 0x78, 0x79, 0x7A};
+const uint8_t show_hour_fuenf[] PROGMEM = {0x67, 0x68, 0x69, 0x6A};
+const uint8_t show_hour_sechs[] PROGMEM = {0x91, 0x92, 0x93, 0x94, 0x95};
+const uint8_t show_hour_sieben[] PROGMEM = {0x55, 0x56, 0x57, 0x58, 0x59, 0x5A};
+const uint8_t show_hour_acht[] PROGMEM = {0x81, 0x82, 0x83, 0x84};
+const uint8_t show_hour_neun[] PROGMEM = {0x73, 0x74, 0x75, 0x76};
+const uint8_t show_hour_zehn[] PROGMEM = {0x85, 0x86, 0x87, 0x88};
+const uint8_t show_hour_elf[] PROGMEM = {0x70, 0x71, 0x72};
+const uint8_t show_hour_zwoelf[] PROGMEM = {0x45, 0x46, 0x47, 0x48, 0x49};
 
 
 
